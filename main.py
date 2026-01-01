02f043605768dea263c9a5d34835fab6c1d529b5
odilee <odilee@MacBook-Air-6.local>
2026-01-01 18:18:12 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "Hello, World!"}
+
+@app.get("/about/")
+def about():
+    return {"message": "This is the about page."}
+
+
