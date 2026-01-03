d1f320ec0bcaf8e84e02513828ce3463087f6d26
odilee <odilee@MacBook-Air-6.local>
2026-01-04 00:31:42 +0600

    Execution and status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from pydantic import BaseModel
 from . import schemas,models
 from .database import engine, SessionLocal
@@ -16,7 +16,7 @@ def get_db():
     finally:
         db.close()
  
-@app.post("/blog", status_code=201)
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.BlogModel, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -29,7 +29,9 @@ def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
 def get_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
     return blog
