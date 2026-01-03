b3f8e10cf01c2f4564d9cb01fb106e15ad51f500
odilee <odilee@MacBook-Air-6.local>
2026-01-03 18:40:34 +0600

    Adding data and getting data from db

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from . import schemas,models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+ 
+@app.post("/blog", status_code=201)
+def create_blog(request: schemas.BlogModel, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog  
 
-@app.post("/blog")
-def create_blog(request: schemas.BlogModel):
-    return request
+@app.get("/blog")
+def get_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
-
+@app.get("/blog/{id}")
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
