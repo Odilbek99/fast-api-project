e5fde3e89c84bf14ac5134de8f1e6c34c1b60148
odilee <odilee@MacBook-Air-6.local>
2026-01-04 14:29:11 +0600

    tags for blogs and userse

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -6,10 +6,10 @@ from .. import schemas, models
 from ..database import get_db
 
 
-router = APIRouter()
+router = APIRouter(tags=["Blogs"])
 
 
-@router.post("/blog", status_code=status.HTTP_201_CREATED, tags=["Blogs"])
+@router.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
@@ -18,7 +18,7 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog  
 
 
-@router.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT,tags=["Blogs"])
+@router.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -31,7 +31,7 @@ def delete_blog(id: int, db: Session = Depends(get_db)):
         )
 
 
-@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED,tags=["Blogs"])
+@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_blog(
     id: int,
     request: schemas.Blog,
@@ -53,12 +53,12 @@ def update_blog(
 
 
 
-@router.get("/blog", response_model=list[schemas.ShowBlog], status_code=status.HTTP_200_OK,tags=["Blogs"])
+@router.get("/blog", response_model=list[schemas.ShowBlog], status_code=status.HTTP_200_OK)
 def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@router.get("/blog/{id}", response_model=schemas.ShowBlog, status_code=status.HTTP_200_OK,tags=["Blogs"])
+@router.get("/blog/{id}", response_model=schemas.ShowBlog, status_code=status.HTTP_200_OK)
 def get_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -6,9 +6,9 @@ from ..database import get_db
 from ..hashing import Hash
 
 
-router = APIRouter()
+router = APIRouter(tags=["Users"])
 
-@router.post("/user",response_model=schemas.ShowUser, status_code=status.HTTP_201_CREATED,tags=["Users"])
+@router.post("/user",response_model=schemas.ShowUser, status_code=status.HTTP_201_CREATED)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     
     new_user = models.User(username=request.username, email=request.email, password=Hash().argon2_hash(request.password))
@@ -18,12 +18,12 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/user", response_model=list[schemas.ShowUser], status_code=status.HTTP_200_OK,tags=["Users"])
+@router.get("/user", response_model=list[schemas.ShowUser], status_code=status.HTTP_200_OK)
 def get_user(db: Session = Depends(get_db)):
     user = db.query(models.User).all()
     return user
 
-@router.get("/user/{id}", response_model=schemas.ShowUser, status_code=status.HTTP_200_OK,tags=["Users"])
+@router.get("/user/{id}", response_model=schemas.ShowUser, status_code=status.HTTP_200_OK)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
